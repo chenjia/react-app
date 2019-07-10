1f91a437335433b5c1073869fa2e2f84e0b591bc
chenjia <251746034@qq.com>
2019-07-10 13:13:00 +0800

    aa

--- src/components/ui/AnimatedRouter.js
+++ src/components/ui/AnimatedRouter.js
@@ -3,28 +3,41 @@ import { TransitionGroup, CSSTransition, SwitchTransition } from 'react-transiti
 
 const historyArray = [];
 class AnimatedRouter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      forward: true
+    };
+  }
+
+  componentWillUpdate(props){
+    if(this.props.location !== props.location){
+      if(historyArray.length === 0){
+        historyArray.push(props.location.pathname);
+      }
+      
+      const back = historyArray.length > 1 && props.location.pathname === historyArray[historyArray.length-2];
+      this.setState({
+        forward: !back
+      }, () => {
+        if(this.state.forward){
+          historyArray.push(props.location.pathname);
+        }else{
+          historyArray.pop();
+        }
+      });
+    }
+  }
+
   render() {
-    let forward = true;
     if(historyArray.length === 0){
       historyArray.push(this.props.location.pathname);
     }
     
-    if(historyArray.length > 1 && this.props.location.pathname === historyArray[historyArray.length-2]){
-      forward = false;
-    }else{
-      forward = true;
-    }
-
-    if(forward){
-      historyArray.push(this.props.location.pathname);
-    }else{
-      historyArray.pop();
-    }
-
     return (
       <SwitchTransition>
-        <CSSTransition classNames="forward" key={this.props.location.pathname} timeout={150}>
-          <div className={'transition-wrapper'} style={{overflow:'hidden auto', width: this.props.screenWidth+'px', height: this.props.screenHeight+'px'}}>{this.props.children}</div>
+        <CSSTransition classNames={this.state.forward?'forward':'back'} key={this.props.location.pathname} timeout={2000}>
+          <div className={'transition-wrapper'} style={{width: this.props.screenWidth+'px', height: this.props.screenHeight+'px'}}>{this.props.children}</div>
         </CSSTransition>
       </SwitchTransition>
     )
